2026c9fee9b54b0f5ba5a8c8ee4df38b8e299fc2
iamsatyanand <iamsatyanandmishra@gmail.com>
2025-07-02 23:48:07 +0530

    response entity

--- src/main/java/com/app/ecom_application/UserController.java
+++ src/main/java/com/app/ecom_application/UserController.java
@@ -1,16 +1,16 @@
 package com.app.ecom_application;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,14 +19,29 @@ public class UserController {
 //        this.userService = userService;
 //    }
 
-    @GetMapping("/api/users")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers(){
+        if (userService.getAllUsers().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
-    @PostMapping("/api/users")
-    public User createUser(@RequestBody User user){
-        return userService.addUser(user);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> GetUserById(@PathVariable Long id){
+        User user = userService.getuserById(id);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//            return new  ResponseEntity<>(user, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(user);
+//        return userService.getuserById(id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
     }
 
 
--- src/main/java/com/app/ecom_application/UserService.java
+++ src/main/java/com/app/ecom_application/UserService.java
@@ -7,14 +7,26 @@ import java.util.List;
 
 @Service
 public class UserService {
-    List<User> userList = new ArrayList<>();
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
 
     public List<User> getAllUsers(){
         return userList;
     }
 
     public User addUser(User user){
+        user.setId(nextId++);
         userList.add(user);
         return user;
+    }
+
+    public User getuserById(Long id) {
+        for(User user : userList){
+            if(user.getId().equals(id)){
+                return user;
+            }
+        }
+        return null; // or throw an exception if user not found
+         // or throw an exception if user not found
     }
 }
